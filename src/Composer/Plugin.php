4b775659cfff6a70f91a5fbba42ad6aa4470cd8b
Dezső BICZÓ <mxr576@gmail.com>
2024-03-14 11:59:18 +0100

    Drop Composer <2.6.0 support
    Closes https://github.com/mxr576/composer-audit-changes/issues/4

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -21,20 +21,8 @@ final class Plugin implements \Composer\Plugin\PluginInterface, \Composer\Plugin
 {
     public const PACKAGE_NAME = 'mxr576/composer-audit-changes';
 
-    private bool $disabled = false;
-
     public function activate(Composer $composer, IOInterface $io): void
     {
-        // This is a runtime check instead of a dependency constraint to allow
-        // installation of this package on projects where composer/composer also
-        // installed as a project dependency (but a global version used instead).
-        if (version_compare($composer::getVersion(), '2.4.0', '<')) {
-            // By pushing this to STDERR we guarantee that the output on
-            // STDOUT (e.g, in JSON) do not get malformed when it is captured.
-            $io->writeError(sprintf('%s is disabled because audit command is only available since Composer 2.4.0. Your version is: %s.', self::PACKAGE_NAME, $composer::getVersion()));
-
-            $this->disabled = true;
-        }
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
@@ -47,10 +35,6 @@ final class Plugin implements \Composer\Plugin\PluginInterface, \Composer\Plugin
 
     public function getCapabilities(): array
     {
-        if ($this->disabled) {
-            return [];
-        }
-
         return [
           'Composer\Plugin\Capability\CommandProvider' => 'mxr576\ComposerAuditChanges\Composer\CommandProvider',
         ];
